d42d9060e24157d68030eab7093f427f9dfd5959
wintage <wintage.wav@gmail.com>
2024-04-03 15:05:47 +0300

    0.2 Congratulations if clicked the ball!

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from pygame.draw import *
 from random import randint
+from math import sqrt
 pygame.init()
 
-FPS = 2
+FPS = 1
 screen = pygame.display.set_mode((1200, 900))
 
 RED = (255, 0, 0)
@@ -15,13 +16,6 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-def new_ball():
-    '''рисует новый шарик '''
-    x = randint(100, 1100)
-    y = randint(100, 900)
-    r = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
 
 def new_ball():
     global x, y, r
@@ -31,9 +25,18 @@ def new_ball():
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
 
-def click(event):
-    print(x, y, r)
+def print_positions(event):
+    print("Ball Pos: ", x, y, r)
+    print("Click Pos: ", event.pos)
 
+def ball_is_clicked(clickPos, ballPos, radius):
+    distance_vec = (clickPos[0] - ballPos[0],
+                    clickPos[1] - ballPos[1])
+    if sqrt(distance_vec[0]*distance_vec[0] + distance_vec[1]*distance_vec[1]) <= radius:
+        print("CONGRATULATIONS")
+        return 1
+    else:
+        return 0
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -47,7 +50,8 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print('Click!')
             if event.button == 1:
-                circle(screen, RED, event.pos, 50)
+                ball_is_clicked(event.pos, (x, y), r)
+                print_positions(event)
                 pygame.display.update()
 
     new_ball()
